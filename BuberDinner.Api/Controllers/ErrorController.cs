83ad47c8fb60b444066dc98f23c7012239db6244
mihail-karabashev <mihailkarabashev@gmail.com>
2022-11-01 14:36:55 +0200

    Add global error handling

---
+++ BuberDinner.Api/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            //get error from the exception where you can find the error from your services
+            Exception? exception = this.HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+            return this.Problem(title: exception?.Message);
+        }
+
+    }
+}
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,6 +1,8 @@
 using BuberDinner.Application.Services.Authentication;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using BuberDinner.Api.Errors;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
@@ -10,10 +12,13 @@ var configuration = builder.Configuration;
         .AddInfrastructure(builder.Configuration);
 
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
